0df5e3393731762072da53bf19d01ba9af913410
Anuj kumar <807anuj@gamil.com>
2026-05-21 01:44:25 +0530

    this  first code of opps

---
+++ Opps/opps.py
@@ -0,0 +1,13 @@
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name,
+        self.age=age
+        
+    def Anuj_kumar(self):
+        print(self)
+        print( self.name, self.age)
+
+p1=Person("anuj",30)
+p1.Anuj_kumar()
+
