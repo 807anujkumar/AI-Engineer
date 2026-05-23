760b97a98279389db1c8a48d277fb42704128aff
Anuj Kumar <anuj.kumar@magicsw.com>
2026-05-23 17:46:04 +0530

    add one class

--- Opps/opps.py
+++ Opps/opps.py
@@ -11,3 +11,11 @@ class Person:
 p1=Person("anuj",30)
 p1.Anuj_kumar()
 
+class anuj(Person):
+    def __init__(self,name,age,gender):
+        super().__init__(name,age)
+        self.gender=gender
+
+
+p2=anuj("Anuj Kumar",30,"Male")
+print(p2.name,p2.age,p2.gender)
